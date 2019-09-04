7a1693eefa5da7c4475f395f69119d3d74f79710
Michael Nieto <mikeand9@gmail.com>
2019-09-04 10:50:56 -0500

    fixed helm installers

--- src/main/java/com/cicd/cicdbackend/controller/HelloWorld.java
+++ src/main/java/com/cicd/cicdbackend/controller/HelloWorld.java
@@ -6,11 +6,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 public class HelloWorld {
 
     @GetMapping("/hi")
-    public ResponseEntity<String> sayHi(@RequestParam(value = "name", required = false) final String name) {
-        return ResponseEntity.ok(" Hi " + (!StringUtils.isBlank(name) ? name : "you") + "!");
+    public ResponseEntity<String> sayHi(@RequestParam(value = "name", required = false) final String name) throws UnknownHostException {
+        String response = " Hi " + (!StringUtils.isBlank(name) ? name : "you") + "!";
+        response += " -> we are in the server " + InetAddress.getLocalHost().getHostName();
+
+        return ResponseEntity.ok(response);
     }
 }
