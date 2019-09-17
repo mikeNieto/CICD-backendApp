1d63b81cea317533394919a3e3d49bda41762d18
Michael Nieto <mikeand9@gmail.com>
2019-09-17 12:03:43 -0500

    changed the endpoint path

--- src/main/java/com/cicd/cicdbackend/controller/HelloWorld.java
+++ src/main/java/com/cicd/cicdbackend/controller/HelloWorld.java
@@ -12,7 +12,7 @@ import java.net.UnknownHostException;
 @RestController
 public class HelloWorld {
 
-    @GetMapping("/hi")
+    @GetMapping("/")
     public ResponseEntity<String> sayHi(@RequestParam(value = "name", required = false) final String name) throws UnknownHostException {
         String response = " Hi " + (!StringUtils.isBlank(name) ? name : "you") + "!";
         response += " -> we are in the server " + InetAddress.getLocalHost().getHostName();
